a459ab667a1e34ee72d2d0122e314f1da10bcd8a
iSplasher <isplasher@gmail.com>
2017-04-22 02:00:47 +0200

    work on website generator;

--- builder.py
+++ builder.py
@@ -1,20 +1,112 @@
-from html import HTML
+import pyhtml
+import tempfile
+from enum import Enum
 
-class CSSElement:
-    "A css element"
-
-    def __init__(self, id, width=0, height=0):
-        self.id = id
-        self.width = width
-        self.width = height
-
-    def text(self):
-        css = "#{} { \n" \
-            + "position: absolute;" \
-            + "width: {}; \n".format(self.width) \
-            + "height: {}; \n".format(self.height) \
-            + "}\n"
-        return css
-
-def build_header():
+class Shape:
     ""
+
+    def __init__(self):
+        self.name = ''
+        self.x = 0
+        self.y = 0
+
+    def css(self):
+        return ""
+
+class Rect(Shape):
+
+    def __init__(self, x, y, w, h):
+        super().__init__()
+        self.name = 'rect'
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def css(self, id):
+        c = "#{} {{ \n".format(id) \
+            + "position: absolute; \n" \
+            + "left: {}; \n".format(self.x) \
+            + "top: {}; \n".format(self.y) \
+            + "width: {}; \n".format(self.w) \
+            + "height: {}; \n".format(self.h) \
+            + "}\n"
+        return c
+
+class Circle(Shape):
+
+    def __init__(self, x, y, r):
+        super().__init__()
+        self.name = 'circle'
+        self.x = x
+        self.y = y
+        self.r = r
+
+class Element:
+    "Et HTML element"
+
+    def __init__(self, id):
+        self.id = id
+        self.css = None
+        self.html = None
+
+
+def build_html(shapes=None):
+    """
+    """
+    assert isinstance(shapes, (tuple, list))
+
+    if not shapes:
+        shapes = []
+        shapes.extend((
+            Rect(0, 0, 800, 500),
+            Rect(0, 510, 800, 300),
+            Rect(0, 810, 800, 300),
+            ))
+
+
+    elements = []
+
+    for n, shape in enumerate(shapes):
+        assert isinstance(shape, Shape)
+        el_id = shape.name+str(n)
+        el = Element(el_id)
+        el.html = pyhtml.div(id=el_id)("content")
+        el.css = shape.css(el_id)
+
+        elements.append(el)
+
+    # byg HTML
+
+    head = pyhtml.head(
+            pyhtml.meta(charset="utf-8"),
+            pyhtml.title("DrawHTML"),
+            pyhtml.style(rel="stylesheet", href="PATH/TO/CSS")
+        )
+    divs = [x.html for x in elements if x.html],
+    body = pyhtml.body(*divs)
+
+    h = pyhtml.html(
+        head,
+        body
+        )
+
+    css = ""
+    for x in elements:
+        css += x.css
+
+    return h, css
+
+
+def generatefiles(html, css, dir="website/static"):
+    "Generates a .html and .css file. Returns tuple with paths"
+
+    files = []
+
+    for content, suffix in ((html.render(), ".html"), (css, ".css")):
+        handle, path = tempfile.mkstemp(suffix, dir=dir)
+        files.append(path)
+        with open(path, "w") as f:
+            f.write(content)
+
+    return tuple(files)
--- main.py
+++ main.py
@@ -1,67 +1,19 @@
 import cv2
-import imutils
 import numpy as np
 
-img = "test4.png"
+filename = 'test5.png'
+img = cv2.imread(filename)
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-image = imutils.resize(cv2.imread(img), width=800) # load image
-modified = image.copy()
-win = cv2.namedWindow("image")
+gray = np.float32(gray)
+dst = cv2.cornerHarris(gray,2,3,0.04)
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # convert to grayscale
-canny = cv2.Canny(gray,60,200) # edge detection
-blurred = cv2.GaussianBlur(gray, (5, 5), 0) # blur slightly to reduce noise
-thresh_value = 0
+#result is dilated for marking the corners, not important
+dst = cv2.dilate(dst,None)
 
+# Threshold for an optimal value, it may vary depending on the image.
+img[dst>0.01*dst.max()]=[0,0,255]
 
-def thresh_change(a):
-    global thresh_value
-    thresh_value = a
-    draw_contour()
-
-cv2.createTrackbar("Thresh", "image", 0, 255, thresh_change)
-
-def draw_contour():
-    global modified
-
-    thresh = cv2.threshold(blurred, thresh_value, 255, cv2.THRESH_BINARY_INV)[1]
-    modified = thresh.copy()
-
-    # find location of white regions using contour detection
-    cnt_img, cnts, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    #cv2.imshow('image', image)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-    # loop over the contours
-    for c in cnts:
-        #M = cv2.moments(c)
-        #cX = int((M["m10"] / M["m00"]))
-        #cY = int((M["m01"] / M["m00"]))
-
-        cv2.drawContours(modified, [c], 0, (0, 255, 0), 2)
-
-
-    #cv2.imshow('image',gray)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-    #cv2.imshow('image',blurred)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-    #cv2.imshow('image',thresh)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-    #cv2.imshow('image',canny)
-    #cv2.waitKey(0)
-    #cv2.destroyAllWindows()
-
-while True:
-    cv2.imshow('image', modified)
-    if 27 == cv2.waitKey(1): # 27 is ESC
-        break
-
-cv2.destroyAllWindows()
+#cv2.imshow('dst',img)
+#if cv2.waitKey(0) & 0xff == 27:
+#    cv2.destroyAllWindows()
